27a86c144950e1bde467afa2d6de77c0767c4964
yuanpengfei <yuanpengfei03@meituan.com>
2021-06-16 15:53:57 +0800

    add

---
+++ src/d.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+    
+<script lang="ts">
+    
+import { Vue, Component, Prop } from "vue-property-decorator";
+    
+@Component
+export default class D extends Vue {}
+    
+</script>
+    
+<style lang="less" scoped>
+    
+</style>
